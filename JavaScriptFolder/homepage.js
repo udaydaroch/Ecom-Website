53ed55c86811e9475cc2b6af914f1e56e01a02b0
Uday <Uda12@uclive.ac.nz>
2023-12-22 14:54:40 +1300

    added a table and options to click  and view photos

--- JavaScriptFolder/homepage.js
+++ JavaScriptFolder/homepage.js
@@ -25,45 +25,84 @@ document.addEventListener("DOMContentLoaded", function () {
     card.appendChild(button_2);
     cardContainer.appendChild(card);
   }
-
   function hideCards() {
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => {
       card.style.display = "none"; // Set display to none
     });
-    if (ClothingCard < 1) { 
-
-        const card = document.createElement("div");
-        card.className = "productcard";
-        cardContainer.appendChild(card);
-        ClothingCard = 1;
-
-  }
-
-    const newButton = document.createElement("button");
-    const parentContainer =  document.createElement("div");
+  
+    if (ClothingCard < 1) {
+      const card = document.createElement("div");
+      card.className = "productcard";
+      cardContainer.appendChild(card);
+      ClothingCard = 1;
+    }
+  
+    const parentContainer = document.createElement("div");
     parentContainer.style.flexDirection = "row";
     parentContainer.style.display = "flex";
-    parentContainer.className = "horizontal-pic-container"; 
+    parentContainer.className = "horizontal-pic-container";
     for (let i = 0; i < 3; i++) {
       const childDiv = document.createElement("div");
       childDiv.className = "photos";
       childDiv.textContent = (i + 1);
-
       parentContainer.appendChild(childDiv);
     }
+  
+    const table = document.createElement("table");
+    table.className = "table";
+    const headerRow = document.createElement("tr");
+    const header1 = document.createElement("th");
+    const header2 = document.createElement("th");
+    const header3 = document.createElement("th");
+  
+    headerRow.appendChild(header1);
+    headerRow.appendChild(header2);
+    headerRow.appendChild(header3);
+  
+    table.appendChild(headerRow);
+  
+    for (let i = 1; i <= 3; i++) {
+      const dataRow = document.createElement("tr");
+      const cell1 = document.createElement("td");
+      cell1.textContent = "Row " + i + ", Column 1";
+      cell1.style.fontSize = "17px"; // Set font size for the cell
+      const cell2 = document.createElement("td");
+      cell2.textContent = "Row " + i + ", Column 2";
+      cell2.style.fontSize = "17px"; // Set font size for the cell
+      const cell3 = document.createElement("td");
+      cell3.textContent = "Row " + i + ", Column 3";
+      cell3.style.fontSize = "17px"; // Set font size for the cell
+  
+      dataRow.appendChild(cell1);
+      dataRow.appendChild(cell2);
+      dataRow.appendChild(cell3);
+  
+      table.appendChild(dataRow);
+    }
+  
+    const tableContainer = document.createElement("div");
+    tableContainer.style.flexDirection = "column";
+    tableContainer.style.display = "flex";
+    tableContainer.appendChild(table);
+  
     cardContainer.appendChild(parentContainer);
-    newButton.textContent = "Show All";
+    cardContainer.appendChild(tableContainer);
+  
+    const newButton = document.createElement("button");
+    newButton.textContent = "Back";
     newButton.className = "BackButton";
+    cardContainer.appendChild(newButton);
     newButton.addEventListener("click", function () {
       showAllCards();
-      cardContainer.removeChild(newButton); 
+      cardContainer.removeChild(newButton);
       cardContainer.removeChild(parentContainer);
+      cardContainer.removeChild(tableContainer);
     });
-
-    cardContainer.appendChild(newButton);
+  
+    
   }
-
+  
   function showAllCards() {
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => {
