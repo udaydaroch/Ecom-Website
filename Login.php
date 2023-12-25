72500bd9a109e15280b949eef3d834926e1a1f62
aabhi09 <80874530+aabhi09@users.noreply.github.com>
2023-12-25 17:06:25 +1300

    Update Login.php

--- Login.php
+++ Login.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Nebula Clothing</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <!-- Include Bootstrap or other styling frameworks if needed -->
     <style>
-        /* Global Styles */
+        /*Global Styles*/ 
         body {
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
             margin: 0;
@@ -14,11 +14,90 @@
             background-color: #f8f9fa;
         }
 
-        /* ... Your existing styles ... */
+        /* Navigation Bar Styles */
+        nav {
+            background-color: #343a40;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            padding: 10px 0;
+        }
 
-        /* Media Query for Responsive Design */
-        @media (max-width: 768px) {
-            /* Add your responsive styles here */
+        .navbar-brand img {
+            max-height: 40px;
+        }
+
+        .navbar-toggler {
+            border: none;
+            outline: none;
+            color: #fff;
+        }
+
+        .navbar-toggler-icon {
+            background-color: #fff;
+        }
+
+        .navbar-nav .nav-link {
+            color: #fff;
+            font-weight: bold;
+            margin: 0 15px;
+            transition: color 0.3s;
+        }
+
+        .navbar-nav .nav-link:hover {
+            color: #ff3366;
+        }
+
+        /* Form Styles */
+        h2 {
+            color: #343a40;
+            text-align: center;
+            padding: 20px 0;
+        }
+
+        .form-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 75vh;
+        }
+
+        .form {
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+            padding: 40px;
+            width: 400px;
+            max-width: 100%;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+            color: #343a40;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            width: 100%;
+            padding: 12px;
+            margin-bottom: 20px;
+            border: 1px solid #ced4da;
+            border-radius: 5px;
+            background-color: #f8f9fa;
+            color: #495057;
+        }
+
+        input[type="submit"] {
+            background-color: #ff3366;
+            color: white;
+            padding: 15px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #e02958;
         }
     </style>
 </head>
@@ -26,7 +105,37 @@
 
     <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg navbar-dark">
-        <!-- ... Your existing navigation bar code ... -->
+        <a class="navbar-brand" href="index.php">
+            <img src="img/nebula_logo.png" width="120" height="40" alt="Nebula Clothing Logo">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="products.php">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About Us</a>
+                </li>
+                <!-- Add more navigation items as needed -->
+
+                <!-- Search Form -->
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </ul>
+
+            <!-- Login and Register Buttons -->
+            <a href="login.php" class="btn btn-light ml-2 custom-login-button">Login</a>
+        </div>
     </nav>
 
     <h2>Nebula Clothing - Login or Sign Up</h2>
@@ -42,7 +151,7 @@
                 <input type="submit" value="Login" class="btn btn-primary custom-login-button">
             </form>
 
-            <!-- Registration Form -->
+            <!--Registration Form-->
             <form action="register_process.php" method="post">
                 <h3>Sign Up</h3>
                 <label for="register-username">Username:</label>
@@ -54,8 +163,8 @@
         </div>
     </div>
 
-    <!-- Include your custom JavaScript files for additional functionality -->
+    
     <script src="js/script.js"></script>
-    <!-- Include Bootstrap or other JavaScript libraries if needed -->
+    
 </body>
 </html>
