4a3dcd66ae640724260965173305652a19e55bab
Uday <Uda12@uclive.ac.nz>
2024-01-05 10:59:10 +1300

    added a addtional notes section for the views to input special instructions etc

--- JavaScriptFolder/Confirmationpage.js
+++ JavaScriptFolder/Confirmationpage.js
@@ -1,8 +1,1 @@
-function disableForm() {
-  var form = document.getElementById('formbox');
-  var elements = form.elements;
 
-  for (var i = 0; i < elements.length; i++) {
-      elements[i].style.display = "None";
-    }
-}
