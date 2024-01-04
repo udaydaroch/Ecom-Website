ee383a116394e7314e11df1c7c1b43ed61499dc3
Uday <Uda12@uclive.ac.nz>
2024-01-05 11:16:20 +1300

    Added a constraint that forces users to choose on of the Order Types.

--- JavaScriptFolder/Confirmationpage.js
+++ JavaScriptFolder/Confirmationpage.js
@@ -1,1 +1,25 @@
+function OrderTypeProcess() {
 
+  
+  var orderType;
+  if(document.getElementById("PickUp").checked)
+  {
+    orderType = 'PickUp';
+  } else if (document.getElementById("Delivery").checked){
+    orderType = 'Delivery';
+
+  }else {
+    alert('Please select an order type')
+    return;
+  }
+
+  if(orderType === "PickUp")
+  {
+    window.location.href ="PickUpConfirmation.html";
+  } else if(orderType === "Delivery") {
+    window.location.href ="DeliveryConfirmation.html";
+  }
+
+
+
+}
