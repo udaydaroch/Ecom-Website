5ac091cc06a6e41055c756289804645a09b967f7
Uday <Uda12@uclive.ac.nz>
2024-01-07 13:38:09 +1300

    made the cards look good

--- index.js
+++ index.js
@@ -1,69 +1,79 @@
 var cardData = {
-  imageSrc: 'shirt1.png',
+  imgSrc: "shirt1.png",
   title: 'India Cricket Shirt',
   description: 'Show your support for the Indian cricket team with our official India Cricket Shirt. Crafted with premium materials and featuring the iconic team colors, this shirt is perfect for cricket enthusiasts and fans alike.',
 };
 
-// Function to generate the card dynamically
+var cardData2 = {
+  imgSrc: "shirt2.png",
+  title: 'Football Jersey',
+  description: 'Manchester United player'
+};
+
+var cardData3 = {
+  imgSrc: "shirt3.png",
+  title: 'Football Jersey',
+  description: 'Versace Football Jersey Special Edition'
+};
+
+var datas = [cardData, cardData2, cardData3];
+
 function generateCard(data) {
-  for (let i= 0; i < 20; i++) {
   var cardContainer = document.getElementById('cardContainer');
 
-  // Create elements
-  var colDiv = document.createElement('div');
-  colDiv.className = 'col-lg-4 col-md-5 col-sm-12';
+  for (let i = 0; i < data.length; i++) {
+      var colDiv = document.createElement('div');
+      colDiv.className = 'col-lg-4 col-md-5 col-sm-12';
 
-  var cardDiv = document.createElement('div');
-  cardDiv.className = 'card mb-5 shadow-sm';
+      var cardDiv = document.createElement('div');
+      cardDiv.className = 'card mb-5 shadow-sm';
 
-  var imgElement = document.createElement('img');
-  imgElement.src = data.imageSrc;
-  imgElement.className = 'img-fluid';
+      var imgElement = document.createElement('img');
+      imgElement.src = data[i].imgSrc;
+      imgElement.className = 'img-fluid';
 
-  var cardBodyDiv = document.createElement('div');
-  cardBodyDiv.className = 'card-body';
+      var cardBodyDiv = document.createElement('div');
+      cardBodyDiv.className = 'card-body';
 
-  var cardTitleDiv = document.createElement('div');
-  cardTitleDiv.className = 'card-title';
+      var cardTitleDiv = document.createElement('div');
+      cardTitleDiv.className = 'card-title';
 
-  var h2Element = document.createElement('h2');
-  h2Element.textContent = data.title;
+      var h2Element = document.createElement('h2');
+      h2Element.textContent = data[i].title;
 
-  var cardTextDiv = document.createElement('div');
-  cardTextDiv.className = 'card-text';
+      var cardTextDiv = document.createElement('div');
+      cardTextDiv.className = 'card-text';
 
-  var pElement = document.createElement('p');
-  pElement.textContent = data.description;
+      var pElement = document.createElement('p');
+      pElement.textContent = data[i].description;
 
-  var buttonDiv = document.createElement('div');
-  buttonDiv.className = 'd-grid gap-2 col-6 mx-auto';
+      var buttonDiv = document.createElement('div');
+      buttonDiv.className = 'd-grid gap-2 col-6 mx-auto';
 
-  var viewButton = document.createElement('button');
-  viewButton.className = 'btn btn-outline-success';
-  viewButton.type = 'button';
-  viewButton.textContent = 'View more';
+      var viewButton = document.createElement('button');
+      viewButton.className = 'btn btn-outline-success';
+      viewButton.type = 'button';
+      viewButton.textContent = 'View more';
 
-  var addToCartButton = document.createElement('button');
-  addToCartButton.className = 'btn btn-outline-primary';
-  addToCartButton.type = 'button';
-  addToCartButton.textContent = 'Add to Cart';
+      var addToCartButton = document.createElement('button');
+      addToCartButton.className = 'btn btn-outline-primary';
+      addToCartButton.type = 'button';
+      addToCartButton.textContent = 'Add to Cart';
 
-  // Append elements to their respective parents
-  cardTitleDiv.appendChild(h2Element);
-  cardTextDiv.appendChild(pElement);
-  buttonDiv.appendChild(viewButton);
-  buttonDiv.appendChild(addToCartButton);
-  cardBodyDiv.appendChild(cardTitleDiv);
-  cardBodyDiv.appendChild(cardTextDiv);
-  cardBodyDiv.appendChild(buttonDiv);
-  cardDiv.appendChild(imgElement);
-  cardDiv.appendChild(cardBodyDiv);
-  colDiv.appendChild(cardDiv);
+      cardTitleDiv.appendChild(h2Element);
+      cardTextDiv.appendChild(pElement);
+      buttonDiv.appendChild(viewButton);
+      buttonDiv.appendChild(addToCartButton);
+      cardBodyDiv.appendChild(cardTitleDiv);
+      cardBodyDiv.appendChild(cardTextDiv);
+      cardBodyDiv.appendChild(buttonDiv);
+      cardDiv.appendChild(imgElement);
+      cardDiv.appendChild(cardBodyDiv);
+      colDiv.appendChild(cardDiv);
 
-  // Append the entire card to the cardContainer
-  cardContainer.appendChild(colDiv);
+      
+      cardContainer.appendChild(colDiv);
+  }
 }
-}
 
-// Call the function with the sample data
-generateCard(cardData);
+generateCard(datas);
